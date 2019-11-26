4c599c0ed06e0b91e8b9ce2801be4bc4bc158282
Janelle88 <jnchristensen1788@gmail.com>
2019-11-26 08:40:11 -0800

    attempting to make a table

--- esm_206_assign_5_jc.R
+++ esm_206_assign_5_jc.R
@@ -1,7 +1,21 @@
 # ------
 # Results A
+# Lauren working on this
 # ------
 
 salamander_df <- read_csv("mack_creek_vertebrates.csv") %>% 
-  clean_names()
+  clean_names() %>% 
+  filter(species %in% c("DITE"))
 
+
+# ------
+# Results B
+# Table
+# ------
+
+channel_class_df <- salamander_df %>% 
+  filter(section %in% c("CC", "OG")) %>% 
+  filter(unittype %in% c("C", "SC", "P"))
+
+channel_class_table <- channel_class_df %>% 
+  pivot_wider()
