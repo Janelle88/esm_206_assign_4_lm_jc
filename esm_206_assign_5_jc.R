794ee5d3e0aba406da8d497ad531dcfaf7b58446
Janelle88 <jnchristensen1788@gmail.com>
2019-11-28 08:06:34 -0800

    some things?

--- esm_206_assign_5_jc.R
+++ esm_206_assign_5_jc.R
@@ -1,4 +1,12 @@
 # ------
+# attach packages
+# ------
+
+library(tidyverse)
+library(janitor)
+library(kableExtra)
+
+# ------
 # Results A
 # Lauren working on this
 # ------
@@ -15,7 +23,16 @@ salamander_df <- read_csv("mack_creek_vertebrates.csv") %>%
 
 channel_class_df <- salamander_df %>% 
   filter(section %in% c("CC", "OG")) %>% 
-  filter(unittype %in% c("C", "SC", "P"))
+  filter(unittype %in% c("C", "SC", "P")) %>% 
+  count(section, unittype)
 
 channel_class_table <- channel_class_df %>% 
-  pivot_wider()
+  pivot_wider(names_from = section,
+              values_from = n)
+
+channel_class_table %>% 
+  kable(col.names = c("Clear Cut", 
+                      "Old Growth")),
+row.names = c("Channel", 
+              "Side-Channel", 
+              "Pool"))
